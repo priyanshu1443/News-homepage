694936f717fca88fd34cd62b7d3b6618c20bf061
Priyanshu farkare <priyanshufarkare@gmail.com>
2022-12-23 18:19:02 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function closenav() {
+    var nav = document.getElementById("closeit")
+    nav.style.visibility = "hidden";
+}
+function opennav() {
+    var nav = document.getElementById("closeit")
+    nav.style.visibility = "visible";
+}
